34a6d689de6470d0e0deff95f31d9d6201f6c822
vladimir <vladimir.segura89@gmail.com>
2022-03-24 18:20:01 -0400

    Configurando STATICFILES_DIRS

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -126,7 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR/'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = BASE_DIR / 'static'
 # // os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
