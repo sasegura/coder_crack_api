4380d3ba36e74ef84ee311fb671199e24921730d
vladimir <vladimir.segura89@gmail.com>
2022-03-24 08:18:20 -0400

    Agregado el fichero '.gitignore' Cambiadas algunas configuraciones

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('DJ_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DJ_DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
-
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('DJ_ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
 
